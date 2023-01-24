2bcdcdda05a18efa38cfcbc6d9fd8e734dc1be3c
Robi <robi.maliqi@hotmail.co.uk>
2023-01-24 19:44:48 +0000

    added confirmation and passowrd columns to users

--- db/schema.rb
+++ db/schema.rb
@@ -10,11 +10,13 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2023_01_24_193819) do
+ActiveRecord::Schema[7.0].define(version: 2023_01_24_194255) do
   create_table "users", force: :cascade do |t|
     t.string "email"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.datetime "confirmed_at"
+    t.string "password_digest", null: false
     t.index ["email"], name: "index_users_on_email", unique: true
   end
 
